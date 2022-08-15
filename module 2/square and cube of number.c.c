870186d7ddb14f095caf992dc9e064b9173dad9c
Khakhariya <110150442+Khakhariya@users.noreply.github.com>
2022-08-15 08:20:25 +0530

    Add files via upload

---
+++ module 2/square and cube of number.c.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main ()
+{
+	int a,b,c;
+	printf("enter your digit to find square and cube:");
+	scanf("%d",&a);
+	b=a*a;
+	printf("your number of square is:%d",b);
+	c=a*a*a;
+	printf("\nyour number of cube is:%d",c);
+}

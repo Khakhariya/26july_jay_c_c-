3dc4c45d5272a8a42fc42b55c3d12c393b953258
Khakhariya <110150442+Khakhariya@users.noreply.github.com>
2022-09-12 09:16:28 +0530

    Add files via upload

---
+++ Task/user defined function.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int m,n;
+int jay (int o)
+{
+	printf("enter the value :- ");
+	scanf("%d",&m);
+}
+int khakhariya ()
+{
+	printf("your value is %d",m);
+}
+int main ()
+{
+	int z;
+    jay (z);
+	khakhariya ();	
+} 
